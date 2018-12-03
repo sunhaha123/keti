70ca01caafd303dc993fe6dc46168ee90759f06e
sunhaha123 <sunjh007@163.com>
2018-12-03 18:02:16 +0800

    训练-生化2

--- apps/analysis/models.py
+++ apps/analysis/models.py
@@ -22,6 +22,7 @@ from events.models import Xiangmu
 class XunlianSh(models.Model):
     xingming = models.ForeignKey(Athlete,related_name='XunlianSh_xingming',verbose_name=u'队员姓名')
     riqi = models.DateField(null=True, blank=True, verbose_name=u'训练日期', default='2010-01-01')
+    leijishijian=models.IntegerField(verbose_name=u'累计训练天数',default=0)
     gaotong = models.FloatField(verbose_name=u'睾酮')
     pizhichun = models.FloatField(verbose_name=u'皮质醇',default=0)
     niaosudan = models.FloatField(verbose_name=u'尿素氮',default=0)
